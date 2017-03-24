196d618fde3c776ef4465eab0c210960a494b7bf
ajlopez <webmaster@ajlopez.com>
2017-03-24 11:36:09 -0300

    XFunction factory method

--- src/ajdraw.js
+++ src/ajdraw.js
@@ -384,6 +384,7 @@ var ajdraw = function() {
 		composite: function (elements, style) { return new Composite(elements, style); },
 		triangle: function (point1, point2, point3, style) { return new Triangle(point1, point2, point3, style); },
 		rectangle: function (from, to, style) { return new Rectangle(from, to, style); },
+		xfunction: function (fn, from, to, style) { return new XFunction(fn, from, to, style ); },
 		
 		Point: Point,
 		Line: Line,
